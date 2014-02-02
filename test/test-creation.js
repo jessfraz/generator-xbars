d5a338b0b4c120f99c0e1c457bdfab92eb4029eb
jfrazelle <jfrazelle@me.com>
2014-02-02 05:35:04 -0500

    tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -22,8 +22,19 @@ describe('jess generator', function () {
     it('creates expected files', function (done) {
         var expected = [
             // add files you expect to exist here.
-            '.jshintrc',
-            '.editorconfig'
+            '.bowerrc',
+            '.gitignore',
+            'app.js',
+            'bower.json',
+            'Gruntfile.js',
+            'package.json',
+            'public/js/bower_components/jquery/jquery.min.js',
+            'public/js/lib/modernizr.js',
+            'public/js/main.js',
+            'public/css/styles.less',
+            'views/404.html',
+            'views/index.html',
+            'views/layouts/main.html'
         ];
 
         helpers.mockPrompt(this.app, {
